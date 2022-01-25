a075f01915f002b3320cc09615aa844ce2111f4c
Nika Pruidze <nika.pruidze1@gmail.com>
2022-01-26 01:03:16 +0400

    create access to ethereum object

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect } from "react";
 import './App.css';
 
-export default function App() {
+const App = () => {
+  const checkIfWalletIsConnected = () => {
+    /*
+    * First make sure we have access to windows.ethereum
+    */
+    const { ethereum } = window;
 
-  const wave = () => {
-    
+    if (!ethereum) {
+      console.log('Make sure you have metamask!');
+    } else {
+      console.log('We have the ethereum object', ethereum);
+    }
   }
-  
+
+  /*
+  * This runs our function when the page loads.
+  */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
         👋 Hey there!
         </div>
-
+        
         <div className="bio">
-        I am Nika and I am getting into web3, that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am Nika and I am getting into web3, that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
+        <button className="waveButton" onClick={null}>
+          Wave at me
         </button>
       </div>
     </div>
   );
 }
+
+export default App
